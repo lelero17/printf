2a9b19ffd28209a676b0e84187b4031afd799120
Leandro Emmerling <leandro-emmerling@web.de>
2025-10-30 15:36:03 +0100

    Add ft_printf.c and printf.h initial implementation

---
+++ printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemmerli <lemmerli@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 13:35:05 by lemmerli          #+#    #+#             */
+/*   Updated: 2025/10/30 13:55:15 by lemmerli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+int	ft_printf(const char*, ...)
+
+#endif
