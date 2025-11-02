61b90ca69a6ccd4cb9faf1a961cd6d875e1ccef1
lelero17 <leandro-emmerling@web.de>
2025-11-02 14:54:20 +0100

    Norm fix

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lemmerli <lemmerli@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 12:44:12 by lemmerli          #+#    #+#             */
-/*   Updated: 2025/11/02 13:33:11 by lemmerli         ###   ########.fr       */
+/*   Updated: 2025/11/02 14:02:00 by lemmerli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,18 @@ int	ft_printf(const char *s, ...)
 
 // int main()
 // {
-// 	ft_printf("Test 1:\n%c, %%, %s, %d, %i, %u, %x, %X, %p\n", 'C', "Teststring", 42, -42, 4294967295u, 255, 255, &ft_printf);
-	
-// 	printf("%c, %%, %s, %d, %i, %u, %x, %X, %p\n\n", 'C', "Teststring", 42, -42, 4294967295u, 255, 255, &ft_printf);
-	
-// 	ft_printf("Test 2:\nINT_MIN: %d | INT_MAX: %d | UINT_MAX: %u | 0: %d | -0: %i\n", INT_MIN, INT_MAX, 4294967295u, 0, -0);
-	
-// 	printf("INT_MIN: %d | INT_MAX: %d | UINT_MAX: %u | 0: %d | -0: %i\n\n", INT_MIN, INT_MAX, 4294967295u, 0, -0);
-
-// 	ft_printf("Test 3:\nNULL string: %s | NULL pointer: %p | Hex 0: %x | Hex MAX: %X\n", (char *)NULL, NULL, 0, 4294967295u);
-
-// 	printf("NULL string: %s | NULL pointer: %p | Hex 0: %x | Hex MAX: %X\n\n", (char *)NULL, NULL, 0, 4294967295u);
-
+// 	ft_printf("Test 1:\n%c, %%, %s, %d, %i, %u, %x, %X, %p\n"
+// 		,'C', "Teststring", 42, -42, 4294967295u, 255, 255, &ft_printf);
+// 	printf("%c, %%, %s, %d, %i, %u, %x, %X, %p\n\n"
+// 		, 'C', "Teststring", 42, -42, 4294967295u, 255, 255, &ft_printf);
+//	ft_printf("Test 2:\n")
+// 	ft_printf("INT_MIN: %d | INT_MAX: %d | UINT_MAX: %u | 0: %d | -0: %i\n"
+// 		, INT_MIN, INT_MAX, 4294967295u, 0, -0);
+// 	printf("INT_MIN: %d | INT_MAX: %d | UINT_MAX: %u | 0: %d | -0: %i\n\n"
+// 		, INT_MIN, INT_MAX, 4294967295u, 0, -0);
+// 	ft_printf("Test 3:\nNULL str: %s | NULL ptr: %p | Hex 0: %x | Hex MAX: %X\n"
+// 		, (char *)NULL, NULL, 0, 4294967295u);
+// 	printf("NULL str: %s | NULL ptr: %p | Hex 0: %x | Hex MAX: %X\n\n"
+// 		, (char *)NULL, NULL, 0, 4294967295u);
 // 	return(0);
 // }
